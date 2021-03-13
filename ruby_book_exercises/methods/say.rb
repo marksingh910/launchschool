407ca45adc361e4ccd06e2bf9b4cd9aee61f75e5
Mark <marksingh910@gmail.com>
2021-03-12 20:09:19 -0800

    Passed strings through method

--- ruby_book_exercises/methods/say.rb
+++ ruby_book_exercises/methods/say.rb
@@ -2,7 +2,16 @@ def say(words)
   puts words
 end
 
+=begin 
+
 puts "Hello."
 puts "Hi."
 puts "How are you?"
 puts "I'm fine."
+
+=end
+
+say("Hello.")
+say("Hi.")
+say("How are you?")
+say("I'm fine.")
