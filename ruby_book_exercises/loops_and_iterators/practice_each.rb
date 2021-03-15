ecf77d9fc59e65708db489d92765fb3223f87149
Mark <marksingh910@gmail.com>
2021-03-14 18:37:32 -0700

    Refactored using each_with_index

--- ruby_book_exercises/loops_and_iterators/practice_each.rb
+++ ruby_book_exercises/loops_and_iterators/practice_each.rb
@@ -1,7 +1,7 @@
 names = ["Bob", "Joe", "Steve", "Susan", "Helen"]
-x = 1
+#x = 1
 
-names.each do |name|
-  puts "#{x}. #{name}"
-  x += 1
+names.each_with_index do |name, index|
+  puts "#{index+1}. #{name}"
+  #x += 1
 end
