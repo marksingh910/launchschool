5e868a4e9da99f01c1803e98be976bd4c3580a22
Mark <marksingh910@gmail.com>
2021-04-08 21:36:38 -0700

    Removed hash folder.

--- ruby_basics/hash/first_car.rb
+++
@@ -1,8 +0,0 @@
-car = {
-  type:    "sedan",
-  color:   "blue",
-  mileage: 80_000
-}
-
-p car
-
--- ruby_basics/hash/labeled_numbers.rb
+++
@@ -1,8 +0,0 @@
-numbers = {
-  high:   100,
-  medium: 50,
-  low:    10
-}
-
-numbers.each_pair {|key, value| puts "A #{key} number is #{value}."}
-
