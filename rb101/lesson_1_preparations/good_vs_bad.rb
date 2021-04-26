65f8fa8767b4f7d2f375bd1446d0e25c7176c517
Mark <marksingh910@gmail.com>
2021-04-26 14:30:13 -0700

    Good vs bad lesson example updated.

--- rb101/lesson_1_preparations/good_vs_bad.rb
+++ rb101/lesson_1_preparations/good_vs_bad.rb
@@ -1,7 +1,11 @@
 NUMBERS = [1, 2, 3]
+arr = [1, 2, 3]
 
 def test
   puts NUMBERS.inspect
+  puts arr.inspect
 end
 
 test
+
+# arr cannot be accessed outside the method. It is not a local variable. NUMBERS being a constant variable, is global.
