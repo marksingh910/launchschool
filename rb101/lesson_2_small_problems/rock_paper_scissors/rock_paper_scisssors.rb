c6ba3554d36c14b92ed25cd694650841096452c9
Mark <marksingh910@gmail.com>
2021-05-05 18:25:01 -0700

    Added bonus features to rock paper scissors game.

--- rb101/lesson_2_small_problems/rock_paper_scissors/rock_paper_scisssors.rb
+++ rb101/lesson_2_small_problems/rock_paper_scissors/rock_paper_scisssors.rb
@@ -1,21 +1,22 @@
-VALID_CHOICES = %w[rock paper scissors lizard spock] #r p s l sp]
+VALID_CHOICES = %w[rock paper scissors lizard spock]
+ABBREVIATED_CHOICES = %w[r p s l sp]
 
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
 def win?(first, second)
-  move_list = { rock:     ['scissors', 'lizard'],
-                paper:    ['rock', 'spock'],
-                scissors: ['paper', 'lizard'],
-                lizard:   ['spock', 'paper'],
-                spock:    ['scissors', 'rock'],
-                # r:        ['scissors', 'lizard'],
-                # p:        ['rock', 'spock'],
-                # s:        ['paper', 'lizard'],
-                # l:        ['spock', 'paper'],
-                # sp:       ['scissors', 'rock']
-                # if using abbr add abbr array ex. lizard: ['spock', 'paper', 'sp', 'p']
+  move_list = {
+    rock: %w[scissors lizard s l],
+    paper: %w[rock spock r sp],
+    scissors: %w[paper lizard p l],
+    lizard: %w[spock paper sp p],
+    spock: %w[scissors rock s r],
+    r: %w[scissors lizard s l],
+    p: %w[rock spock r sp],
+    s: %w[paper lizard p l],
+    l: %w[spock paper sp p],
+    sp: %w[scissors rock s r]
   }
   move_list[first.to_sym].include?(second)
 end
@@ -30,13 +31,17 @@ def display_results(player, computer)
   end
 end
 
+player_wins = 0
+computer_wins = 0
 loop do
   choice = ''
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    prompt(" Choose one: #{VALID_CHOICES.join(', ')}
+    Shortcuts: r - rock, p - paper, s - scissors, l - lizard, sp - spock")
+
     choice = Kernel.gets().chomp()
 
-    if VALID_CHOICES.include?(choice)
+    if VALID_CHOICES.include?(choice) || ABBREVIATED_CHOICES.include?(choice)
       break
     else
       prompt('Choice not valid.')
@@ -48,6 +53,24 @@ loop do
   prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
 
   display_results(choice, computer_choice)
+
+  if win?(choice, computer_choice)
+    player_wins += 1
+  elsif win?(computer_choice, choice)
+    computer_wins += 1
+  end
+
+  prompt("Player wins: #{player_wins}, Computer wins: #{computer_wins}")
+
+  if player_wins == 5
+    prompt("Player is the grand champion")
+    player_wins = 0
+    computer_wins = 0
+  elsif computer_wins == 5
+    prompt("Computer is the grand champion")
+    player_wins = 0
+    computer_wins = 0
+  end
 
   prompt('Do you want to play again?')
 
